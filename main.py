4352edd522cea4001954da8f6df85ecfc8d4771c
ruiroros <s10239922@connect.np.edu.sg>
2022-07-28 15:39:10 +0800

    rui

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-api_key = "0PXEE709XYMK7M42"
-url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}"
-
-response = requests.get(url)
-data = response.json()
-print(type(data))
