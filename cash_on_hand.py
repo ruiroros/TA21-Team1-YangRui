002ec3b8668ffe4e4127c457df4644d0502c9871
yoohphoric <vandachong17@gmail.com>
2022-07-28 16:54:24 +0800

    vanda

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 # cwd now is np one drive/"IGP PFB"/"project_group"/"cash_on_hand.py"
 fp = Path.cwd()
-cash_fp = fp/"cash-on-hand-usd.csv"
+cash_fp = fp/"project_group"/"csv_reports"/"cash-on-hand-usd.csv"
 print(cash_fp)
-print(fp)
+
