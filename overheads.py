bdfcb974215e1c23c9e97f79842e087fea5a506e
ruiroros <s10239922@connect.np.edu.sg>
2022-07-28 16:40:53 +0800

    rui

--- overheads.py
+++ overheads.py
@@ -1,1 +1,12 @@
-print("try")
+from pathlib import Path
+fp = Path.cwd()
+overheads_fp = fp/"overheads-45.csv"
+
+print(fp)
+print(overheads_fp)
+
+with overheads_fp.open(mode='r', encoding='UTF-8') as file:
+    overheads_read = overheads_fp.read()
+
+print(overheads_fp)
+
