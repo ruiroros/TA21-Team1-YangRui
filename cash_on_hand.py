0ea7ec8e2eb75053c2d78ebd04c0a2adc4b69c24
ruiroros <s10239922@connect.np.edu.sg>
2022-07-30 00:45:49 +0800

    r

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -6,6 +6,7 @@ coh_fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 def cash_on_hand_function():
     cash_on_hand = []
     days = []
+    diff = []
 
     with coh_fp.open(mode='r', encoding='UTF-8', newline="") as file:
         cash_read = csv.reader(file)
@@ -19,17 +20,18 @@ def cash_on_hand_function():
             days.append(day)
         
         x = 1
-        while x < len(cash_on_hand):
+        for difference in cash_on_hand:
             difference = float(cash_on_hand[x]) - float(cash_on_hand[x-1])
-            # print(cash_on_hand[x-1])
-            # print(cash_on_hand[x])
-            #print(difference)
+
+# for students in polys_info:
+#             empty_list.append(students[1])
+
             x += 1
             if difference < 0:
                 message = f"[CASH DEFICIT] DAY:{days[x-1]}, AMOUNT: #need convert?? "
             else: 
+                #i think this is wrong 
                 message = f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY "
             print(message)
 
 cash_on_hand_function()
-
