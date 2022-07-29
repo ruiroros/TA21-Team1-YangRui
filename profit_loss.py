a8c5aebb30b712b83f700dd966f7cc23f0ac9ba2
ruiroros <s10239922@connect.np.edu.sg>
2022-07-29 15:54:58 +0800

    r

--- profit_loss.py
+++ profit_loss.py
@@ -3,12 +3,20 @@ import csv
 
 profitloss_fp = Path.cwd/"csv_reports"/"profit-and-loss-usd.csv"
 
-with profitloss_fp.open(mode='r', encoding='UTF-8') as file:
-    profitloss_read = file.read()
+with profitloss_fp.open(mode='r', encoding='UTF-8', newline="") as file:
+    profitloss_read = csv.reader(file)
     next(profitloss_read)
 
     for line in profitloss_read:
-        profitloss = line[4]
-        print(profitloss)
+        profitloss = line[5]
+        print(line[5])
 
+####
+# with cash_fp.open(mode='r', encoding='UTF-8', newline="") as file:
+#     cash_read = csv.reader(file)
+#     next(cash_read)
+
+#     for line in cash_read:
+#         cash_on_hand = line[1]
+#         print(type(cash_on_hand))
 
