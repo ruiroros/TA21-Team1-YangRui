26ce7f8e676015e6cf915e4d8d96675102d0035d
ruiroros <s10239922@connect.np.edu.sg>
2022-07-29 23:58:08 +0800

    R

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -5,6 +5,7 @@ coh_fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
 
 def cash_on_hand_function():
     cash_on_hand = []
+    days = []
 
     with coh_fp.open(mode='r', encoding='UTF-8', newline="") as file:
         cash_read = csv.reader(file)
@@ -13,6 +14,9 @@ def cash_on_hand_function():
         for line in cash_read:
             coh = line[1]
             cash_on_hand.append(coh)
+
+            day = line[0]
+            days.append(day)
         
         x = 1
         while x < len(cash_on_hand):
@@ -22,10 +26,9 @@ def cash_on_hand_function():
             #print(difference)
             x += 1
             if difference < 0:
-                message = "test only"
+                message = f"[CASH DEFICIT] DAY:{days[x-1]}, AMOUNT: #need convert?? "
             else: 
-                message = "ok"
-
+                message = f"[CASH SURPLUS] CASH ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY "
             print(message)
 
 cash_on_hand_function()
