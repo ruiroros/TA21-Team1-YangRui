9b5e0f71947faf5aa5b7b7b25e50454b6f1726d2
ruiroros <s10239922@connect.np.edu.sg>
2022-07-29 15:21:24 +0800

    r

--- profit_loss.py
+++ profit_loss.py
@@ -1,13 +1,15 @@
-import csv
 from pathlib import Path
-fp = Path.cwd()
-profitloss_fp = fp/"csv_reports"/"profit-and-loss-usd.csv"
+import csv 
 
-print(fp)
-print(profitloss_fp)
+profitloss_fp = Path.cwd/"csv_reports"/"profit-and-loss-usd.csv"
 
 with profitloss_fp.open(mode='r', encoding='UTF-8') as file:
     profitloss_read = file.read()
+    next(profitloss_read)
+
+    for line in profitloss_read:
+        profitloss = line[]
+        print()
 
 print(profitloss_read)
 
