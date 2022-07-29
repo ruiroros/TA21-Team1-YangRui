27d76e98e730bd5bb0d24bb9e69ff9a4be287acd
yoohphoric <vandachong17@gmail.com>
2022-07-29 16:23:32 +0800

    vanda

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -9,11 +9,13 @@ with cash_fp.open(mode='r', encoding='UTF-8', newline="") as file:
     cash_read = csv.reader(file)
     next(cash_read)
 
+empty_list = []
 def diff_in_cash_values(final_value):
     for line in cash_read:
         cash_on_hand = int(line[1])
-        print(cash_on_hand)
-        final_value = cash_on_hand
+        empty_list.append(cash_on_hand)
+    final_value = empty_list[0]-empty_list[1]
+    print(final_value)
         # for days, ca`sh in enumerate(line):
         #     print(cash)
         # maybe use def function with return?
