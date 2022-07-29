0833eb00ee212eb4f2dd81da819ba1850c832d48
ruiroros <s10239922@connect.np.edu.sg>
2022-07-29 15:32:23 +0800

    u

---
+++ marco.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import csv
+
+# cwd now is np one drive/"IGP PFB"/"project_group"/"cash_on_hand.py"
+cash_fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+# print(cash_fp)
+
+with cash_fp.open(mode='r', encoding='UTF-8', newline="") as file:
+    cash_read = csv.reader(file)
+    next(cash_read)
+
+    for line in cash_read:
+        cash_on_hand = float(line[1])
+        print((cash_on_hand)- float(enumerate(line)))
+        # for days, cash in enumerate(line):
+        cash_on_hand = line[1]
+        print(cash_on_hand)
+        # for days, ca`sh in enumerate(line):
+        #     print(cash)6d2
+
+#why tf got the days number!!! how to get rid pls
+
