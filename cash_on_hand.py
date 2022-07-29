a62c51de81d4d1876fb43afddcb95d5ee18a6815
ruiroros <s10239922@connect.np.edu.sg>
2022-07-29 10:52:06 +0800

    rui

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,7 +2,6 @@ from pathlib import Path
 # cwd now is np one drive/"IGP PFB"/"project_group"/"cash_on_hand.py"
 fp = Path.cwd()
 cash_fp = fp/"csv_reports"/"cash-on-hand-usd.csv"
-print(fp)
 print(cash_fp)
 
 with cash_fp.open(mode='r', encoding='UTF-8') as file:
@@ -10,4 +9,12 @@ with cash_fp.open(mode='r', encoding='UTF-8') as file:
 
 print(cash_read)
 
-# append to empty list?? enumerate and use re.search(pattern='[0-9][0-9][0-9][0-9][0-9][0-9][0-9]', string = index)/findall?
+# vanda : append to empty list?? enumerate and use re.search(pattern='[0-9][0-9][0-9][0-9][0-9][0-9][0-9]', string = index)/findall?
+
+# rui starts from line 15 TRY 
+
+cash_string = str(cash_read)
+print(cash_string)
+# print(type(cash_string))
+print(cash_string[2])
+
---
+++ yrTESTINGfolder.py
@@ -0,0 +1,6 @@
+phrase = 'Your name is andy and your age is 12'
+
+find = phrase.find(13)
+print(find)
+# replace `andy` with tom using .replace() 
+print(phrase.replace('andy', 'tom'))
