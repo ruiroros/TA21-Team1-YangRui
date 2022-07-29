24d5aa1d9bc08a1e1d9b45ceb6988f8ff67dd97b
yoohphoric <vandachong17@gmail.com>
2022-07-29 15:45:15 +0800

    vanda

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -10,7 +10,7 @@ with cash_fp.open(mode='r', encoding='UTF-8', newline="") as file:
     next(cash_read)
 
     for line in cash_read:
-        cash_on_hand = line[1]
+        cash_on_hand = line[1].split()
         print(type(cash_on_hand))
         # for days, ca`sh in enumerate(line):
         #     print(cash)
