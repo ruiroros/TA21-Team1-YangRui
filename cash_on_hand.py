0205d61611d0c74f9ff5ad04df99d973a696e854
ruiroros <s10239922@connect.np.edu.sg>
2022-07-29 11:32:43 +0800

    r

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,13 +1,16 @@
 from pathlib import Path
+import csv
+
 # cwd now is np one drive/"IGP PFB"/"project_group"/"cash_on_hand.py"
-fp = Path.cwd()
-cash_fp = fp/"csv_reports"/"cash-on-hand-usd.csv"
-print(cash_fp)
+cash_fp = Path.cwd()/"csv_reports"/"cash-on-hand-usd.csv"
+# print(cash_fp)
 
-with cash_fp.open(mode='r', encoding='UTF-8') as file:
-    cash_read = file.read()
+with cash_fp.open(mode='r', encoding='UTF-8', newline="") as file:
+    cash_read = csv.reader(file)
+    next(cash_read)
 
-print(cash_read)
+    for line in cash_read:
+        print(line)
 
 # vanda : append to empty list?? enumerate and use re.search(pattern='[0-9][0-9][0-9][0-9][0-9][0-9][0-9]', string = index)/findall?
 
