4fc1bec96480b07f0372ba0cdf99a6aec7810121
marcoyuen12 <marcoyuen56@gmail.com>
2022-07-29 15:23:20 +0800

    m

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -10,9 +10,9 @@ with cash_fp.open(mode='r', encoding='UTF-8', newline="") as file:
     next(cash_read)
 
     for line in cash_read:
-        cash_on_hand = (line[1])
-        print(cash_on_hand)
-        # for days, ca`sh in enumerate(line):
+        cash_on_hand = float(line[1])
+        print((cash_on_hand)- float(enumerate(line)))
+        # for days, cash in enumerate(line):
         #     print(cash)
 
 #why tf got the days number!!! how to get rid pls
