b68e322a671b17ae68c5dac2053cedf5189c1cf6
yoohphoric <vandachong17@gmail.com>
2022-08-02 15:06:18 +0800

    v

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import api, cash_on_hand, overheads, profit_loss
 
 def main():
     forex = api.api_function()
-    overheads.overhead_function(forex)
+    overheads.overheads_function(forex)
     cash_on_hand.cash_on_hand_function(forex)
     profit_loss.profit_loss_function(forex)
 
