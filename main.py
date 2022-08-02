1783c2d4a6aae6e72f62fe668dfd8de59d02c6c4
ruiroros <s10239922@connect.np.edu.sg>
2022-08-02 22:42:09 +0800

    r

--- main.py
+++ main.py
@@ -10,11 +10,12 @@ def main():
 main()
 
 ##SECOND WAY ? 
+
 # import api, coh, overheads, profit_loss
 
 # def main():
 
-#     forex = api.api_function(forex)
+#     forex = api.api_function()
 #     overheads.overhead_function(forex)
 #     coh.coh_function(forex)
 #     profit_loss.profitloss_function(forex)
