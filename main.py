45d62f8806fec865417c9d26a65ea789ae13ec5e
ruiroros <s10239922@connect.np.edu.sg>
2022-08-04 22:31:53 +0800

    r

--- main.py
+++ main.py
@@ -5,17 +5,23 @@ def main_function():
     fp = Path.cwd()/'summary_report.txt'
     fp.touch()
     
+    # forex = api.api_function()
+    # one = overheads.overhead_function()
+    # two = coh.coh_function()
+    # thr = profit_loss.profitloss_function()
+
     forex = api.api_function()
     one = overheads.overhead_function()
     two = coh.coh_function()
     thr = profit_loss.profitloss_function()
 
 
-    with fp.open(mode='w', encoding='UTF-8', newline= '') as file:
-        file.writelines(forex)
-    with fp.open(mode='a', encoding='UTF-8', newline= '') as file:
-        file.writelines(f"{one}")
-        file.writelines(f"{two}")
-        file.writelines(f"{thr}")
+
+    # with fp.open(mode='w', encoding='UTF-8', newline= '') as file:
+    #     file.writelines(forex)
+    # with fp.open(mode='a', encoding='UTF-8', newline= '') as file:
+    #     file.writelines(f"{one}")
+    #     file.writelines(f"{two}")
+    #     file.writelines(f"{thr}")
 main_function()
 
